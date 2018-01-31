12482392a800d92187258a266ebe1d45183bb671
quentinjaub <33456178+quentinjaub@users.noreply.github.com>
2018-01-31 23:14:42 +0100

    rotation curve

--- Code/approx.py
+++ Code/approx.py
@@ -5,7 +5,7 @@ import math
 import numpy as np
 from scipy.misc import comb
 from scipy import interpolate
-from pylab import subplots_adjust,subplot
+import pylab as plb
 from plot_save import normalize
 
 
@@ -115,8 +115,8 @@ def need_symmetric(angle_yaw,angle_pitch,angle_roll,id_curve):
 
 def need_symmetric_pitch_yaw(angle_x,angle_y):
 	positive_l,negative_l = [],[]
-	positive_l = [angle_x[i] for i in range(0,len(angle_y)) if angle_y[i]>0.5]
-	negative_l = [angle_x[i] for i in range(0,len(angle_y)) if angle_y[i]<0.5]
+	positive_l = [angle_y[i] for i in range(0,len(angle_y)) if angle_x[i]>0.5]
+	negative_l = [angle_y[i] for i in range(0,len(angle_y)) if angle_x[i]<0.5]
 	if(max(positive_l)>max(negative_l)):
 		return False
 	else:
@@ -124,32 +124,44 @@ def need_symmetric_pitch_yaw(angle_x,angle_y):
 
 def need_symmetric_roll_pitch(angle_x,angle_y):
 	positive_l,negative_l = [],[]
-	positive_l = [angle_y[i] for i in range(0,len(angle_y)) if angle_y[i]>0.5]
-	negative_l = [angle_y[i] for i in range(0,len(angle_y)) if angle_y[i]<0.5]
+	positive_l = [angle_x[i] for i in range(0,len(angle_y)) if angle_y[i]>0.5]
+	negative_l = [angle_x[i] for i in range(0,len(angle_y)) if angle_y[i]<0.5]
 	if(max(positive_l)>max(negative_l)):
 		return False
 	else:
 		return True
 
 def need_symmetric_yaw_roll(angle_x,angle_y):
-	return False
+	"""
+	positive_l,negative_l = [],[]
+	positive_l = [angle_y[i] for i in range(0,len(angle_y)) if angle_y[i]>0.51]
+	negative_l = [angle_y[i] for i in range(0,len(angle_y)) if angle_y[i]<0.51]
+	"""
+	l = [angle_y[i] for i in range(0,len(angle_y)) if angle_x[i]>0.49 and angle_x[i]<0.51]
+	if(np.min(l)<0.5):
+		return False
+	else:
+		return True
 	
 	
 def adapt_orientation(angle_yaw,angle_pitch,angle_roll,id_curve):
 
 	if(need_symmetric(angle_yaw,angle_pitch,angle_roll,id_curve)):
+		print "symmetric " + str(id_curve)
 		if (id_curve==1):
-			return angle_yaw,[y * -1 for y in angle_pitch],angle_roll
+			return [y * -1 +1 for y in angle_yaw],angle_pitch,angle_roll
 		elif (id_curve==2):
-			return angle_yaw,angle_pitch,[y * -1 for y in angle_roll]
+			return angle_yaw,angle_pitch,[y * -1 +1 for y in angle_roll]
 		else:
-			return angle_yaw,angle_pitch,angle_roll
+			return [y * -1 +1 for y in angle_yaw],angle_pitch,[y * -1 +1 for y in angle_roll]
 		 
 	else:
 		return angle_yaw,angle_pitch,angle_roll
 
+
 def plot_example(list_path,id_curve):
 	yaw_l,pitch_l,roll_l = [],[],[]
+	list_name = []
 	for path in list_path:	
 		yaw,pitch,roll  = get_file_data(path)
 		yaw,pitch,roll  = normalize(yaw,pitch,roll)
@@ -157,38 +169,46 @@ def plot_example(list_path,id_curve):
 		yaw_l.append(yaw)
 		pitch_l.append(pitch)
 		roll_l.append(roll)
-
+		list_name.append(path.split('/')[1])
+	control_x,control_y = [],[]
 	if(id_curve==1): #pitch = f(yaw)
 		angle_x = yaw_l
 		angle_y = pitch_l
 		title   = 'pitch = f(yaw)'	
-		control_x,control_y = get_control_points(yaw_l[0],pitch_l[0])
-
+		c_x,c_y = get_control_points(yaw_l[0],pitch_l[0])
+		control_x.append(c_x)
+		control_y.append(c_y)
 	elif(id_curve==2):#roll = f(pitch)
 		angle_x = pitch_l
 		angle_y = roll_l
 		title   = 'roll = f(pitch)'
-		control_x,control_y = get_control_points(pitch_l[0],roll_l[0])
+		c_x,c_y = get_control_points(pitch_l[0],roll_l[0])
+		control_x.append(c_x)
+		control_y.append(c_y)
 	else:
 		angle_x = roll_l
 		angle_y = yaw_l
 		title   = 'yaw = f(roll)'
-		control_x,control_y = get_control_points(roll_l[0],yaw_l[0])
-	
-	tck,u=interpolate.splprep([control_x,control_y],s=0.0)
+		c_x,c_y = get_control_points(roll_l[0],yaw_l[0])
+		control_x.append(c_x)
+		control_y.append(c_y)
+
+	tck,u=interpolate.splprep([control_x[0],control_y[0]],s=0.0)
 	x_spline,y_spline= interpolate.splev(np.linspace(0,1,100),tck)	
-
+	
 	#Plot
-	subplots_adjust(hspace=0.000)
+	plb.rcParams['figure.figsize'] = 25, 10
+	plb.subplots_adjust(hspace=0.5,wspace=0.5)
 	number_of_subplots=len(list_path)
-
+	
 	for i,v in enumerate(xrange(number_of_subplots)):
 	    v = v+1
-	    ax1 = subplot(number_of_subplots,1,v)
+	    ax1 = plb.subplot(number_of_subplots/2+1,2,v)
 	    ax1.plot(angle_x[i],angle_y[i],'r--',x_spline,y_spline)
-	    ax1.set_title(title + ' : Jeu de donnees ' + str(i))
-	    ax1.set_xlim([0,1])
-	    ax1.set_ylim([0,1])
+	    ax1.set_title(title + ' : ' + list_name[i] + ' (Spline 1) ')
+	    ax1.set_xlim([-0.5,1.5])
+	    ax1.set_ylim([-0.5,1.5])
+	
 	plt.show()
 
 #####################################################################
@@ -200,8 +220,34 @@ name_2 = "bonnemaison_elodie_22"
 
 file_3 = "Fri Oct  6 17_57_57 2017 - Lacet.orpl"
 name_3 = "Marine_Lepetit_22"
-list_path = ['bonnes_mesures/'+name_1+'/'+file_1,'bonnes_mesures/'+name_2+'/'+file_2,'bonnes_mesures/'+name_3+'/'+file_3]
+path_4 = 'cimia_karen_22/Wed Sep 27 16_46_59 2017 - Lacet.orpl'
+path_5 = 'De bortoli_Marion_23/Wed Dec  6 16_33_22 2017 - Lacet.orpl'
+path_6 = 'roma_mathieu_22/Fri Dec  8 13_47_53 2017 - Lacet.orpl'
 
-plot_example(list_path,1)
-plot_example(list_path,2)
+list_path = ['bonnes_mesures/'+name_1+'/'+file_1,'bonnes_mesures/'+name_2+'/'+file_2,'bonnes_mesures/'+name_3+'/'+file_3,'bonnes_mesures/'+path_4,'bonnes_mesures/'+path_5,'bonnes_mesures/'+path_6]
+"""
+y,p,r = get_file_data('bonnes_mesures/'+path_5)
+y,p,r  = normalize(y,p,r)
+plt.plot(y,p)
+plt.xlim(-0.5,1.5)
+plt.ylim(-0.5,1.5)
+plt.show()
+y,p,r  = adapt_orientation(y,p,r,1)
+plt.plot(y,p)
+plt.show()
+
+plt.plot(p,r)
+plt.xlim(-0.5,1.5)
+plt.ylim(-0.5,1.5)
+plt.show()
+plt.plot(r,y)
+plt.xlim(-0.5,1.5)
+plt.ylim(-0.5,1.5)
+plt.show()
+
+
+"""
+#plot_example(list_path,1)
+#plot_example(list_path,2)
 plot_example(list_path,3)
+
