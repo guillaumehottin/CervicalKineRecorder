a9305b902dd3d15e8b263d2d7277202b9d060916
guillaumehottin <guillaume.hottin@etu.enseeiht.fr>
2018-02-22 15:36:56 +0100

    fonctionnel

--- Cervical_GUI/main.py
+++ Cervical_GUI/main.py
@@ -4,17 +4,18 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 from view.my_window import MyWindow
 import subprocess
 
-# try:
-subprocess.Popen([r"C:\Users\Florian\Documents\projet_long\projetlong\Cervical_GUI\cervical.exe"])
-# except OSError:
-    # print("Application 'Cervical' Oculus non trouvée")
+try:
+    subprocess.Popen([r"..\Cervical_exec\cervical.exe"])
+    app = QApplication(sys.argv)
+    window = QMainWindow()
+    ui = MyWindow(window)
+    #
+    window.show()
+    sys.exit(app.exec_())
+except OSError:
+    print("Application 'Cervical' Oculus non trouvée")
 
-app     = QApplication(sys.argv)
-window  = QMainWindow()
-ui      = MyWindow(window)
-#
-window.show()
-sys.exit(app.exec_())
+
 
 
 
